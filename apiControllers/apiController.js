c18b239076029950b13491a7971318230eb62aec
GerGab <ger.gab.gonzalez@gmail.com>
2022-04-29 20:46:33 +0200

    correcciones desafio3

--- apiControllers/apiController.js
+++ apiControllers/apiController.js
@@ -1,11 +1,12 @@
-const { products } = require("../dataBases/products.js")
+const { fileContainer } = require("../dataBases/fileContainer.js")
+const prodContainer = new fileContainer("./dataBases/products.txt"); 
 
 const apiController =  {
     products: async (req, res) => {
-        res.json( await products.getAll());
+        res.json( await prodContainer.getAll())
     },
     random: async (req, res) => {
-        res.json( await products.getRandom())
+        res.json( await prodContainer.getRandom())
     }
 }
 
--- dataBases/fileContainer.js
+++ dataBases/fileContainer.js
@@ -1,53 +1,56 @@
 const fs = require('fs');
-const file = "./dataBases/products.txt"
 
-const products =  {
+class fileContainer {
+
+    constructor(file){
+        this.file = file;
+    }
 
     async save(object){
         let objects = await this.readFromFile();
         objects.push(object);
         await this.writeInFile(objects)
-    },
+    }
 
     async getById(id){
         let objects = await this.readFromFile()
         let object = objects.find(object => object.id === id)
         return object ?? null  
-    },
+    }
 
     async getRandom(){
         let objects = await this.readFromFile()
         let index = Math.trunc(Math.random()*objects.length)
         return objects[index]
-    },
+    }
 
     async getAll(){
         let objects = await this.readFromFile()
         return objects
-    },
+    }
 
     async deleteById(id){
         const objects =  await this.readFromFile()
         const filterObjects = objects.filter(item => item.id !== id);
         await this.writeInFile(filterObjects)
-    },
+    }
 
     async deleteAll(){
         await this.writeInFile([])
-    },
+    }
 
     async writeInFile(content){
         try{
-            await fs.promises.writeFile(file,JSON.stringify(content,null,2))
+            await fs.promises.writeFile(this.file,JSON.stringify(content,null,2))
         }
         catch(err){
             console.log(`Error en escritura: ${err}`)
         }
-    },
+    }
 
     async readFromFile(){
         try{
-            let content = await fs.promises.readFile(file)
+            let content = await fs.promises.readFile(this.file)
             return JSON.parse(content)
         }
         catch(err){
@@ -56,4 +59,4 @@ const products =  {
     }
 }
 
-module.exports = {products}
+module.exports = {fileContainer}
